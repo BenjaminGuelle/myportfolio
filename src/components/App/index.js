1270e621e3127022db24e917714e9da1e9bf1513
Benjamin Guelle <benjamin.guelle@gmail.com>
2021-06-02 16:33:35 +0200

    create integration to home view done

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,4 +1,4 @@
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 
 import './style.scss';
 
@@ -9,8 +9,16 @@ import Nav from '../Nav';
 function App() {
   return (
     <div className="App">
+      <Link to="/" className="backhome">benjaminguelle.com</Link>
+      <div className="social">
+        <Link to="/" className="social-linkedin social-txt">Linkedin</Link>
+        <Link to="/" className="social-github social-txt">Github</Link>
+        <Link to="/" className="social-twitter social-txt">Twitter</Link>
+      </div>
+      <div className="copyright">2021 &copy; BG</div>
       <Nav />
       <Route path="/">
+        <Home />
       </Route>
     </div>
   );
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,33 +1,31 @@
+import { Link } from 'react-router-dom';
 import './style.scss';
+import moi from '../../assets/images/nav-picture.jpg'
 
 function Nav() {
     return (
         <nav className="nav">
-            <div className="links scrollable">
-                <div>
-                    <span>
-                        Me connaitre
-                    </span>
-                </div>
-                <div>
-                    <span>
-                        Compétences
-                    </span>
-                </div>
-                <div>
-                    <span>
-                        Portfolio
-                    </span>
-                </div>
-                <div>
-                    <span>
-                        Home
-                    </span>
-                </div>
-            </div>
+            <ul className="scrollable">
+                <li className="scrollable-item">
+                    <Link to="#" className="scrollable-item-link">Contact</Link>
+                </li>
+                <li className="scrollable-item">
+                    <Link to="#" className="scrollable-item-link">Me connaître</Link>
+                </li>
+                <li className="scrollable-item">
+                    <Link to="#" className="scrollable-item-link">Compétences</Link>
+                </li>
+                <li className="scrollable-item">
+                    <Link to="#" className="scrollable-item-link">Portfolio</Link>
+                </li>
+                <li className="scrollable-item">
+                    <Link to="#" className="scrollable-item-link">Accueil</Link>
+                </li>
+                <div className="scrollable-selected" />
+            </ul>
 
             <div className="nav-picture">
-
+                <img src={moi} alt="photo-de-benjamin" />
             </div>
         </nav>
     )
